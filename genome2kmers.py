12f6d788752162e42941beeb36be5249d28619ab
Ignacio Arroyo <axayakac@gmail.com>
2018-06-29 18:36:29 -0500

    first commitment

---
+++ genome2kmers.py
@@ -0,0 +1,10 @@
+# >1 dna:chromosome chromosome:AnoCar2.0:1:1:263920458:1 REF
+# GTGTATTCGAATGATATAAACAATAGAAATAAGCAGTAGAAAACATTTGATATAGGACGA
+# AGTTCACAACATCTGGGGAATCCAATATAGAAGTGGGGTTCAAGCAGCATGATTTCACAA
+# ...
+# >AAWZ...
+
+dna_file = "../data/lagartija/Anolis_carolinensis.AnoCar2.0.dna.chromosome.1.fa"
+dna_f = open(dna_file)
+
+
